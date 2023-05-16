0a044871fe8ae92c231acfbe23c8f303df1fdfa0
0lgatron <perronolga@gmail.com>
2023-05-16 16:26:00 -0400

    config express server, routes, controller

---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,30 @@
+
+
+
+const getAllTasks = (req, res) => {
+  res.send('get all items')
+}
+
+const getTask = (req, res) => {
+  res.send('get single task')
+}
+
+const createTask = (req, res) => {
+  res.send('create task')
+}
+
+const updateTask = (req, res) => {
+  res.send('get single task')
+}
+
+const deleteTask = (req, res) => {
+  res.send('delete task')
+}
+
+module.exports = {
+  getAllTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+}
